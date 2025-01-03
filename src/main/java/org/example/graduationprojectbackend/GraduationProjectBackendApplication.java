92133847ff2d569ce3c62a26d8d38ed9c3c6bf4d
lentee <yuxinyang2003@126.com>
2025-01-03 17:21:54 +0800

    Initial commit

---
+++ src/main/java/org/example/graduationprojectbackend/GraduationProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.graduationprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/graduationprojectbackend/GraduationProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.graduationprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
